68f268e50f84f3e9332a9d698b3a255dac9a4456
Felipe Pieretti Umpierre <umpierre.felipe@gmail.com>
2015-09-17 03:36:32 +0000

    First commit

---
+++ app/Http/routes.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * API 1.0
+ *
+ * @author Felipe Pieretti Umpierre
+ */
+Route::prefix(["prefix" => "api"], function() {
+
+    /**
+     * Insert a new order
+     */
+    Route::post("/order", function() {
+
+    });
+
+    /**
+     * Update an order
+     */
+    Route::put("/order/{id}", function($id) {
+
+    });
+
+    /**
+     * Get information from an order
+     */
+    Route::get("/order/{id}", function($id) {
+
+    });
+
+    /**
+     * Get the menu list
+     */
+    Route::get("menu", function() {
+
+    });
+
+});
