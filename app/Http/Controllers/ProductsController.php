c78063cc31cda18021f93e18dc20c6752ec3e021
Felipe Pieretti Umpierre <umpierre.felipe@gmail.com>
2015-10-10 20:56:24 +0000

    Controller Products to handle all products requests

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Products;
+use App\Extensions\ProductsTrait;
+use Illuminate\Support\Facades\Response;
+use Symfony\Component\Debug\Exception\FatalErrorException;
+
+class ProductsController extends Controller
+{
+    use ProductsTrait;
+
+    /**
+     * Get the product
+     *
+     * @param int $id product id
+     * @return json
+     */
+    public function index($id)
+    {
+        return Response::json(Products::find($id) ?: [
+            'message' => 'error to find product. Check if is the correct product.',
+            'return' => null,
+        ]);
+    }
+
+    /**
+     * List the ingredients from the product
+     *
+     * @param int $id product id
+     * @return json
+     */
+    public function ingredients($id)
+    {
+        return Response::json(Products::find($id)->ingredients()->get() ?: [
+            'message' => 'error to find the ingredients from this product.',
+            'return' => null,
+        ]);
+    }
+
+    /**
+     * List the products with ingredients and category.
+     *
+     * If an error occurred, a FatalErrorException will be throws.
+     *
+     * @see App\Exceptions\Handler
+     * @throws FatalErrorException
+     * @return json
+     */
+    public function menu()
+    {
+        $products = Products::all();
+
+        $this->addProductsListsCollection($products, ['ingredients', 'category']);
+
+        return Response::json($products);
+    }
+}
