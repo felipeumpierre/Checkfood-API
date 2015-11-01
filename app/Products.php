d64899cba9380e43c7a97f9babe18319f9ed0c42
Felipe Pieretti Umpierre <umpierre.felipe@gmail.com>
2015-11-01 15:29:24 +0000

    Added fileUpload relation

--- app/Products.php
+++ app/Products.php
@@ -9,7 +9,7 @@ class Products extends Model
     /**
      * @var array
      */
-    protected $hidden = ['created_at', 'updated_at', 'pivot', 'file_upload_id', 'categories_id', 'stock'];
+    protected $hidden = ['created_at', 'updated_at', 'file_upload_id', 'categories_id', 'stock'];
 
     /**
      * @var array
@@ -22,6 +22,14 @@ class Products extends Model
     public function category()
     {
         return $this->belongsTo('App\Categories', 'categories_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function fileUpload()
+    {
+        return $this->belongsTo('App\FileUpload', 'file_upload_id');
     }
 
     /**
