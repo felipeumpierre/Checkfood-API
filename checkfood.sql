76edd98af888d4c2de2ef6c1ce9ec6b85d539efa
Felipe Pieretti Umpierre <umpierre.felipe@gmail.com>
2015-11-01 15:31:41 +0000

    Current database

--- checkfood.sql
+++ checkfood.sql
@@ -22,8 +22,22 @@ DROP TABLE IF EXISTS `checkfood`.`categories` ;
 CREATE TABLE IF NOT EXISTS `checkfood`.`categories` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NULL,
-  `created_at` DATETIME NULL,
-  `updated_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` DATETIME CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `checkfood`.`file_upload`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `checkfood`.`file_upload` ;
+
+CREATE TABLE IF NOT EXISTS `checkfood`.`file_upload` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(95) NULL,
+  `created_at` DATETIME CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
@@ -36,17 +50,24 @@ DROP TABLE IF EXISTS `checkfood`.`products` ;
 CREATE TABLE IF NOT EXISTS `checkfood`.`products` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `categories_id` INT NOT NULL,
+  `file_upload_id` INT NOT NULL,
   `name` VARCHAR(150) NULL,
   `description` TEXT NULL,
   `price` DECIMAL(10,2) NULL,
   `stock` INT NULL,
-  `created_at` DATETIME NULL,
-  `updated_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` DATETIME CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,,
   PRIMARY KEY (`id`),
   INDEX `fk_products_categories1_idx` (`categories_id` ASC),
+  INDEX `fk_products_file_upload1_idx` (`file_upload_id` ASC),
   CONSTRAINT `fk_products_categories1`
     FOREIGN KEY (`categories_id`)
     REFERENCES `checkfood`.`categories` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_products_file_upload1`
+    FOREIGN KEY (`file_upload_id`)
+    REFERENCES `checkfood`.`file_upload` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -60,8 +81,22 @@ DROP TABLE IF EXISTS `checkfood`.`boards` ;
 CREATE TABLE IF NOT EXISTS `checkfood`.`boards` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `number` VARCHAR(45) NULL,
-  `created_at` DATETIME NULL,
-  `updated_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` DATETIME CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `checkfood`.`status`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `checkfood`.`status` ;
+
+CREATE TABLE IF NOT EXISTS `checkfood`.`status` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `created_at` DATETIME CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
@@ -73,24 +108,21 @@ DROP TABLE IF EXISTS `checkfood`.`requests` ;
 
 CREATE TABLE IF NOT EXISTS `checkfood`.`requests` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `products_id` INT NOT NULL,
   `boards_id` INT NOT NULL,
-  `unity_price` DECIMAL(10,2) NULL,
-  `quantity` INT NULL,
-  `total_price` DECIMAL(10,2) NULL,
-  `created_at` DATETIME NULL,
-  `updated_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `status_id` INT NOT NULL,
+  `created_at` DATETIME CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,,
   PRIMARY KEY (`id`),
-  INDEX `fk_requests_products_idx` (`products_id` ASC),
   INDEX `fk_requests_boards1_idx` (`boards_id` ASC),
-  CONSTRAINT `fk_requests_products`
-    FOREIGN KEY (`products_id`)
-    REFERENCES `checkfood`.`products` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+  INDEX `fk_requests_status1_idx` (`status_id` ASC),
   CONSTRAINT `fk_requests_boards1`
     FOREIGN KEY (`boards_id`)
     REFERENCES `checkfood`.`boards` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_requests_status1`
+    FOREIGN KEY (`status_id`)
+    REFERENCES `checkfood`.`status` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -111,26 +143,51 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `checkfood`.`products_ingredients`
+-- Table `checkfood`.`requests_products`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `checkfood`.`products_ingredients` ;
+DROP TABLE IF EXISTS `checkfood`.`requests_products` ;
 
-CREATE TABLE IF NOT EXISTS `checkfood`.`products_ingredients` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `checkfood`.`requests_products` (
+  `id` INT NOT NULL,
+  `requests_id` INT NOT NULL,
   `products_id` INT NOT NULL,
-  `ingredients_id` INT NOT NULL,
-  `created_at` DATETIME NULL,
+  `unity_price` DECIMAL(10,2) NULL,
+  `quantity` INT(3) NULL,
+  `total_price` DECIMAL(10,2) NULL,
+  `created_at` DATETIME CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  INDEX `fk_products_ingredients_ingredients1_idx` (`ingredients_id` ASC),
-  INDEX `fk_products_ingredients_products1_idx` (`products_id` ASC),
-  CONSTRAINT `fk_products_ingredients_ingredients1`
-    FOREIGN KEY (`ingredients_id`)
-    REFERENCES `checkfood`.`ingredients` (`id`)
+  INDEX `fk_requests_products_requests1_idx` (`requests_id` ASC),
+  INDEX `fk_requests_products_products1_idx` (`products_id` ASC),
+  CONSTRAINT `fk_requests_products_requests1`
+    FOREIGN KEY (`requests_id`)
+    REFERENCES `checkfood`.`requests` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_products_ingredients_products1`
+  CONSTRAINT `fk_requests_products_products1`
     FOREIGN KEY (`products_id`)
     REFERENCES `checkfood`.`products` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `checkfood`.`requests_observation`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `checkfood`.`requests_observation` ;
+
+CREATE TABLE IF NOT EXISTS `checkfood`.`requests_observation` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `requests_products_id` INT NOT NULL,
+  `observation` TEXT NULL COMMENT '	\n',
+  `created_at` DATETIME CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  INDEX `fk_requests_observation_requests_products1_idx` (`requests_products_id` ASC),
+  CONSTRAINT `fk_requests_observation_requests_products1`
+    FOREIGN KEY (`requests_products_id`)
+    REFERENCES `checkfood`.`requests_products` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
