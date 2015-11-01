bea82c31376ac8df7f01336612a70de385349593
Felipe Pieretti Umpierre <umpierre.felipe@gmail.com>
2015-11-01 15:29:59 +0000

    Added pivot in belongsToMany relation

--- app/Requests.php
+++ app/Requests.php
@@ -6,21 +6,37 @@ use Illuminate\Database\Eloquent\Model;
 
 class Requests extends Model
 {
+    /**
+     * @var array
+     */
     protected $hidden = ['created_at', 'updated_at'];
+
+    /**
+     * @var array
+     */
     protected $fillable = ['status_id', 'boards_id'];
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function board()
     {
         return $this->belongsTo('App\Boards', 'boards_id');
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function status()
     {
         return $this->belongsTo('App\Status', 'status_id');
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function products()
     {
-        return $this->belongsToMany('App\Products', 'requests_products')->withTimestamps();
+        return $this->belongsToMany('App\Products', 'requests_products')->withPivot('id', 'unity_price', 'quantity', 'total_price')->withTimestamps();
     }
 }
