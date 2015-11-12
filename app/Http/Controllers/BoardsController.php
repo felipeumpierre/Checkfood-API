b51f8f1098660dcd73591a187ec511ca9afbe4c4
Felipe Pieretti Umpierre <umpierre.felipe@gmail.com>
2015-11-12 02:37:02 +0000

    Created all action to list all the boards with the status, using the generic ListTrait

--- app/Http/Controllers/BoardsController.php
+++ app/Http/Controllers/BoardsController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Boards;
+use App\Extensions\ListsTrait;
 use App\Http\Requests;
 use Illuminate\Support\Facades\Response;
 
 class BoardsController extends Controller
 {
+    use ListsTrait;
+
     /**
      * Get the board
      *
@@ -20,5 +23,19 @@ class BoardsController extends Controller
             'message' => 'error to find board. Check if is the correct board.',
             'return' => null,
         ]);
+    }
+
+    /**
+     * Get all boards
+     *
+     * @return json
+     */
+    public function all()
+    {
+        $boards = Boards::all();
+
+        $this->addListsCollection($boards, ['status']);
+
+        return Response::json($boards);
     }
 }
